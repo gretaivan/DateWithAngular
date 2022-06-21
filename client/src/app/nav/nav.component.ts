cb9b21c3eb825162906dd4c302d1680f5b67f8a4
gretaivan <greta.ivan91@gmail.com>
2022-06-21 21:50:21 +0100

    adds toastr error pop ups for login and register screens

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs/internal/Observable';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,20 +14,22 @@ export class NavComponent implements OnInit {
 
   model: any = {}
 
-  constructor(public accountService: AccountService) { } //passes services to the nav component
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { } //passes services to the nav component
   ngOnInit(): void {
     console.log(this.accountService.currentUser$)
   }
 
   login(){
     this.accountService.login(this.model).subscribe(res => {
-      console.log(res); 
+      this.router.navigateByUrl('/members')
     }, error => {
       console.log(error);
+      this.toastr.error(error.error); 
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/')
   }
 }
